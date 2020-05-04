930894db5c6baed1e282e2d04260c7ede1c44137
loiphin <simonbonham@gmail.com>
2020-05-04 10:10:44 +0200

    Add files via upload

--- aes256-cbc.py
+++ aes256-cbc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # A poor mans implementation of AES256-CBC. This is just for fun. 
-# If you want a proper AES library, use something like PyCrytodome.
+# If you want a proper AES library, use something like PyCryptodome.
 # 
 #
 # by Simon Bonham - May 2020
